26b5cc6bd2570ff317b2ce0817e695792d1e51d9
Hamdani Dinul Haq <dinulhaqhamdani41@gmail.com>
2021-12-13 23:15:21 +0700

    finish slicing

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,20 @@
+import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm'
+
+export default function MealItem (props) {
+  const { name, description, price } = props
+  const priceFormat = `$${price.toFixed(2)}`
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{name}</h3>
+        <div className={classes.description}>{description}</div>
+        <div className={classes.price}>{priceFormat}</div>
+      </div>
+      <div>
+        <MealItemForm />
+      </div>
+    </li>
+  )
+}
--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,12 +1,17 @@
+import classes from './MealsSummary.module.css'
+
 export default function MealsSummary () {
   return (
-    <section>
+    <section className={classes.summary}>
       <h2>Delicious food, Deliver To You</h2>
       <p>
         Choose your favourite meal from our board selection of available meals
         and enjoy a delicious lunch or dinner at home.
       </p>
-      <p>All our meals</p>
+      <p>
+        All our meals are cooked with high-quality ingredients, just in time and
+        of course by experienced chefs!
+      </p>
     </section>
   )
 }
